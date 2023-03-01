5ecf34d93855fd4769c60985121ba90a4dfcbfdc
FubuGold <124594648+FubuGold@users.noreply.github.com>
2023-03-01 21:36:23 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Giải phương trình bậc nhất')
+a = st.number_input('Tham số a')
+b = st.number_input('Tham số b')
+
+if st.button('Giải'):
+    if a != 0: st.success('Phương trình có 1 nghiệm ' + str(-b/a))
+    else: st.error(s = 'Phương trình vô nghiệm')
